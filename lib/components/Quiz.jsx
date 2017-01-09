63bec20c2976ad800152a23fca136d177ff88e8e
gness1804 <graham.nessler@gmail.com>
2017-01-08 17:01:21 -0700

    API call successfully gets quizzes

--- lib/components/Quiz.jsx
+++ lib/components/Quiz.jsx
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 
 class Quiz extends Component {
 
+  makeAPICall(){
+  var hitAPI = new XMLHttpRequest();
+  hitAPI.open('GET', 'http://localhost:3001/quizzes', true);
+  hitAPI.send();
+  hitAPI.onreadystatechange = function() {
+    if (hitAPI.readyState === XMLHttpRequest.DONE) {
+      if (hitAPI.status === 200) {
+        console.log(JSON.parse(hitAPI.responseText));
+      } else {
+        console.error('There was a problem with the API call.');
+      }
+    }
+  }
+  }
+
   render() {
     return (
-      <div>I am the Quiz component.</div>
+      <div onLoad={ this.makeAPICall() }>
+        
+      </div>
     );
   }
 
