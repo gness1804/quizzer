8e113df12275f2f7592d90014449103e40acb7d8
gness1804 <graham.nessler@gmail.com>
2017-01-08 17:31:12 -0700

    Finally sets the state of title

--- lib/components/Quiz.jsx
+++ lib/components/Quiz.jsx
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 
 class Quiz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+  }
 
   makeAPICall(){
-  var hitAPI = new XMLHttpRequest();
-  hitAPI.open('GET', 'http://localhost:3001/quizzes', true);
-  hitAPI.send();
-  hitAPI.onreadystatechange = function() {
-    if (hitAPI.readyState === XMLHttpRequest.DONE) {
-      if (hitAPI.status === 200) {
-        console.log(JSON.parse(hitAPI.responseText));
-      } else {
-        console.error('There was a problem with the API call.');
+    let that = this;
+    var hitAPI = new XMLHttpRequest();
+    hitAPI.open('GET', 'http://localhost:3001/quizzes', true);
+    hitAPI.send();
+    hitAPI.onreadystatechange = function() {
+      if (hitAPI.readyState === XMLHttpRequest.DONE) {
+        if (hitAPI.status === 200) {
+          let result = JSON.parse(hitAPI.responseText);
+          let singleQuiz = result.quizzes[0];
+          that.setState({ title: singleQuiz.title });
+        } else {
+          console.error('There was a problem with the API call.');
+        }
       }
     }
   }
-  }
 
   render() {
+
+    const { title } = this.state;
+
     return (
-      <div onLoad={ this.makeAPICall() }>
-        
+      <div>
+        <button onClick={() => { this.makeAPICall() }}>Click</button>
+        {title}
       </div>
     );
   }
